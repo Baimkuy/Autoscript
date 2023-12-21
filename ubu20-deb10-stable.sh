83723825b5a0c7f394d489ce92e93f86f09f2a42
Baimkuy <143687687+Baimkuy@users.noreply.github.com>
2023-12-21 23:22:07 +0700

    Update ubu20-deb10-stable.sh

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -532,7 +532,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit IP & Quota"
-wget -q https://raw.githubusercontent.com/Baimkuy/Autoscript/GM//config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
+wget -q https://raw.githubusercontent.com/Baimkuy/Autoscript/GM/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
 
 # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
